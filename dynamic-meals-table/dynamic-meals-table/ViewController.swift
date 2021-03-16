c31cd2da843f4db9e1380700442733011284c39c
Elisa Camillo <elisatcamillo@gmail.com>
2021-03-16 13:37:12 -0300

    :sparkles: creating table cells

--- dynamic-meals-table/dynamic-meals-table/ViewController.swift
+++ dynamic-meals-table/dynamic-meals-table/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    let meals = ["cake", "beans", "rice", "noodles"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("did load")
-        
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        return cell
+    }
+    
     
 //    func viewDidLoad(){
 //      "Overriding declaration requires an 'override' keyword"
